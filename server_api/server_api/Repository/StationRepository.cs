e7630feecc92fee290b38e5fe353e1ca5f60d6de
Zach Lobato <zacharyisaiahlobato@hotmail.com>
2016-02-05 09:38:52 -0700

    Fixed but that was not allowing Station/Parameter references in query

--- server_api/server_api/Repository/StationRepository.cs
+++ server_api/server_api/Repository/StationRepository.cs
@@ -36,16 +36,47 @@ namespace server_api
             return data;
         }
 
-        public IQueryable GetLatestDataPointsFromStation(string stationID)
+        public IEnumerable<DataPoint> GetLatestDataPointsFromStation(string stationID)
         {
-            var data = (from points in db.DataPoints
-                                          where points.Station.Id == stationID
-                                          group points by points.Parameter.Name into paramPoints
-                                          select new
-                                          {
-                                              dataPoints = paramPoints.OrderByDescending(a => a.Time).FirstOrDefault()
-                                          });
+            IEnumerable<DataPoint> data = (from points in db.DataPoints
+                        where points.Station.Id == stationID
+                        group points by points.Parameter.Name into paramPoints
+                        select new
+                        {
+                            dataPoints = paramPoints.OrderByDescending(a => a.Time).FirstOrDefault()
+                        }).Select(c => c.dataPoints).ToList();
+
             return data;
+        }
+
+        public bool SetDataPointsFromStation(DataPoint[] dataSet)
+        {
+            string stationId = dataSet[0].Station.Id;
+            Station dataSetStation = db.Stations.SingleOrDefault(s => s.Id == stationId);
+            
+            if (dataSetStation == null)
+            {
+                return false;
+            }
+
+            foreach(DataPoint point in dataSet){
+                db.Parameters.Attach(point.Parameter);
+                point.Station = dataSetStation;
+            }
+
+            db.DataPoints.AddRange(dataSet);
+
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            return true;
         }
 
         public IEnumerable<DataPoint> GetDataPointsFromStation(string stationID)
