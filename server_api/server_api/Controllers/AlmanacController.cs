78f4237f52f8e09b7d70225deed9f35637a1f66c
Zach Lobato <zacharyisaiahlobato@hotmail.com>
2016-02-16 17:58:17 -0700

    basic almanac controller

---
+++ server_api/server_api/Controllers/AlmanacController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Web.Http;
+using System.Web.Http.Description;
+using server_api.Models;
+using System.Web.Script.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace server_api.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class AlmanacController : ApiController
+    {
+        private StationsRepository _repo = null;
+
+        public AlmanacController()
+        {
+            _repo = new StationsRepository();
+        }
+    }
+}
