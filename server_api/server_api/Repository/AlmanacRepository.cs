57893438bf7483d292fcf013009877bbb54bcae1
Zach Lobato <zacharyisaiahlobato@hotmail.com>
2016-02-17 17:57:56 -0700

    Added Almanac Repository

---
+++ server_api/server_api/Repository/AlmanacRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity.SqlServer;
+
+namespace server_api
+{
+
+
+    public class AlmanacRepository : IDisposable
+    {
+        private AirUDBCOE db;
+
+        public AlmanacRepository()
+        {
+            db = new AirUDBCOE();
+        }
+
+        public AlmanacRepository(AirUDBCOE existingContext)
+        {
+            db = existingContext;
+        }
+
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+    }
+}
