0f87f90530635006ff74ac2f43fb0fe52752d236
Jared Potter <u0729256@utah.edu>
2016-02-28 14:09:00 -0700

    Contact Us email endpoint done. Ready for review.

--- server_api/server_api/Controllers/ContactUsController.cs
+++ server_api/server_api/Controllers/ContactUsController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Mail;
 using System.Web.Http;
+using server_api.Models;
 
 namespace server_api.Controllers
 {
@@ -14,18 +16,23 @@ namespace server_api.Controllers
         /// </summary>
         [Route("contactUs")]
         [HttpPost]
-        public IHttpActionResult SendEmail([FromBody]Content content)
+        public IHttpActionResult SendEmail([FromBody]EmailContent content)
         {
+            string body = "Name: " + content.Name + '\n' +
+                          "Subject: " + content.Subject + '\n' +
+                          "Email: " + content.Email + '\n' +
+                          "Message: " + content.Message;
 
-            return Ok("yeah bitches");
+            var client = new SmtpClient("smtp.gmail.com", 587)
+            {
+                Credentials = new NetworkCredential("air.utah.cs@gmail.com", "Burri0s123*"),
+                EnableSsl = true,
+
+            };
+            client.Send(content.Email, "air.utah.cs@gmail.com", content.Subject, body);
+            Console.WriteLine("Sent");
+
+            return Ok();
         }
-    }
-
-    public class Content
-    {
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Subject { get; set; }
-        public string Message { get; set; }
     }
 }
