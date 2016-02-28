5463d060cd9995fcdab9a2e061d592d9b2dbc188
JDoge <u0729256@utah.edu>
2016-02-28 12:00:04 -0700

    Please let this work.

---
+++ server_api/server_api/Controllers/ContactUsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace server_api.Controllers
+{
+    public class ContactUsController : ApiController
+    {
+        /// <summary>
+        ///   Sends the post contents to airu group email address.
+        /// </summary>
+        [Route("contactUs")]
+        [HttpPost]
+        public IHttpActionResult SendEmail([FromBody]Content content)
+        {
+
+            return Ok("yeah bitches");
+        }
+    }
+
+    public class Content
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
