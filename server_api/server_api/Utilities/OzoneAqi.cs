c67675cc0dc658fcb3841c5c41ae48d55435a69f
Jared Potter <u0729256@utah.edu>
2016-03-31 22:26:42 -0600

    Mised some files...

--- server_api/server_api/Utilities/OzoneAqi.cs
+++ server_api/server_api/Utilities/OzoneAqi.cs
@@ -18,49 +18,38 @@ namespace server_api.Utilities
 
         public static Tuple<Tuple<double, double>, Tuple<int, int>> BreakPoints(double value)
         {
-            if (value <= 12.0)
-            {
-                Tuple<double, double> BP = new Tuple<double, double>(0.0, 12.0);
-                Tuple<int, int> I = new Tuple<int, int>(0, 50);
-                return new Tuple<Tuple<double, double>, Tuple<int, int>>(BP, I);
-            }
+            // 1 hour Ozone AQI values are only generated for values above 100.
+            // Pg. 14 - https://www3.epa.gov/ttn/caaa/t1/memoranda/rg701.pdf
 
-            if (value <= 35.4)
+            if (value <= 0.164)
             {
-                Tuple<double, double> BP = new Tuple<double, double>(12.1, 35.4);
-                Tuple<int, int> I = new Tuple<int, int>(51, 100);
-                return new Tuple<Tuple<double, double>, Tuple<int, int>>(BP, I);
-            }
-
-            if (value <= 55.4)
-            {
-                Tuple<double, double> BP = new Tuple<double, double>(35.5, 55.4);
+                Tuple<double, double> BP = new Tuple<double, double>(0.125, 0.164);
                 Tuple<int, int> I = new Tuple<int, int>(101, 150);
                 return new Tuple<Tuple<double, double>, Tuple<int, int>>(BP, I);
             }
 
-            if (value <= 150.4)
+            if (value <= 0.204)
             {
-                Tuple<double, double> BP = new Tuple<double, double>(55.5, 150.4);
+                Tuple<double, double> BP = new Tuple<double, double>(0.165, 0.204);
                 Tuple<int, int> I = new Tuple<int, int>(151, 200);
                 return new Tuple<Tuple<double, double>, Tuple<int, int>>(BP, I);
             }
 
-            if (value <= 250.4)
+            if (value <= 0.404)
             {
-                Tuple<double, double> BP = new Tuple<double, double>(150.5, 250.4);
+                Tuple<double, double> BP = new Tuple<double, double>(0.205, 0.404);
                 Tuple<int, int> I = new Tuple<int, int>(201, 300);
                 return new Tuple<Tuple<double, double>, Tuple<int, int>>(BP, I);
             }
 
-            if (value <= 350.4)
+            if (value <= 0.504)
             {
-                Tuple<double, double> BP = new Tuple<double, double>(250.5, 350.4);
+                Tuple<double, double> BP = new Tuple<double, double>(0.405, 0.504);
                 Tuple<int, int> I = new Tuple<int, int>(301, 400);
                 return new Tuple<Tuple<double, double>, Tuple<int, int>>(BP, I);
             }
 
-            return new Tuple<Tuple<double, double>, Tuple<int, int>>(new Tuple<double, double>(350.5, 500), new Tuple<int, int>(401, 500));
+            return new Tuple<Tuple<double, double>, Tuple<int, int>>(new Tuple<double, double>(0.505, 0.604), new Tuple<int, int>(401, 500));
         }
     }
 }
