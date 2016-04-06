82f1cbe6c7abceaa361c1c2737653a72af9e01e7
TJHooker33 <rolyat.wilson@gmail.com>
2016-04-06 00:05:39 -0600

    removes old comments

--- server_api/server_api/Models/UserPreferences.cs
+++ server_api/server_api/Models/UserPreferences.cs
@@ -26,12 +26,8 @@
 
         public String DefaultDownloadFormat { get; set; }
 
-        // TODO: have this Fkey to Station table so I don't have to check on update
         public String DefaultStationId { get; set; }
 
-        // This is where the problem is. How does a user get more than 1 parameter?
-        // This adds a user_id column to Parameter table, but then once a parameter is
-        // assigned to a user, no one else can use the parameter.
         public virtual ICollection<Parameter> DefaultParameters { get; set; }
     }
 }
